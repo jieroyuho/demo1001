2370789cc708856ef2046d22a4595a965f6909e2
Tsung-Yu Ho <jieroyuho@gmail.com>
2016-10-01 14:27:52 +0800

    ng g c article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HandleDirective } from './handle.directive';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HandleDirective
+    HandleDirective,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
