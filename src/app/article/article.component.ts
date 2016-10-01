6689cc1889cddd242ff6d7cb9855fa672951c75a
Tsung-Yu Ho <jieroyuho@gmail.com>
2016-10-01 17:05:49 +0800

    在 ArticleComponent 注入 DataService 元件

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,28 +1,22 @@
 import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit, OnChanges {
+export class ArticleComponent implements OnInit {
 
   @Input() item: any;
-  //@Input() item2: any;
-  @Output() deleteOut = new EventEmitter<any>();
-  constructor() { } // 一開始執行
 
-  ngOnInit() {   // 當所有的input 和 output bing後就會開始執行
+  constructor(private datasvc: DataService) { };
+
+  ngOnInit() {
   }
 
-  ngOnChanges() { // 當input 有變化開始執行
-
-  }
-  ngDoCheck(){}  // 不要用，有變更偵測時，但效能會很差
-
-  ngOnDestroy(){} //元件可被摧毀，但是事件不會，尤其被註冊的事件在元件消失後還沒消失
 
   doDelete(item) {
-    this.deleteOut.emit(item);
+    this.datasvc.doDeleteArticle(item);
   }
 }
