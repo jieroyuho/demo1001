819a590bacc62d9d3f95059ef4efc909961065de
Tsung-Yu Ho <jieroyuho@gmail.com>
2016-10-01 14:35:30 +0800

    @Input

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+
+  @Input()
+  item: any;
 
   constructor() { }
 
